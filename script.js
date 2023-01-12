6b1a2a16ab4ca15dacee6f4a685284de2f64ed40
Kreoniz <kreoniz@mail.ru>
2023-01-12 16:12:52 +0600

    Create a function linking operator signs with corresponding functions

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, firstNumber, secondNumber) {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+    }
+}
