d1a6e87a4f568f374cbef9d6638e886841a99834
Kreoniz <kreoniz@mail.ru>
2023-01-12 16:44:20 +0600

    Make operations calculate the value

--- script.js
+++ script.js
@@ -28,13 +28,30 @@ function operate(operator, firstNumber, secondNumber) {
 }
 
 let displayValue = "";
+let value = 0;
+let operation = "";
 
 const display = document.querySelector("#display");
 const digitButtons = document.querySelectorAll(".digit");
+const operatorButtons = document.querySelectorAll(".operator");
 
 digitButtons.forEach(digit => {
     digit.addEventListener("click", e => {
         displayValue += e.currentTarget.textContent;
         display.textContent = displayValue;
+    });
+});
+
+operatorButtons.forEach(operator => {
+    operator.addEventListener("click", e => {
+        if (operation) {
+            value = operate(operation, value, displayValue);
+            display.textContent = value;
+            displayValue = "";
+        } else {
+            value = +displayValue;
+            displayValue = "";
+        }
+        operation = e.currentTarget.textContent;
     });
 });
