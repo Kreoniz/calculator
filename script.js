3bfaa27cae7cc5e3cbb67863ab86b2ebf1b7bce4
Kreoniz <kreoniz@mail.ru>
2023-01-14 16:39:58 +0600

    Fix: leading zero no longer shows up on display

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const operatorButtons = document.querySelectorAll(".operator");
 
 digitButtons.forEach(digit => {
     digit.addEventListener("click", e => {
-        displayValue += e.currentTarget.textContent;
+        displayValue = +(displayValue + e.currentTarget.textContent);
         display.textContent = displayValue;
     });
 });
