632c2f84a627ca30bfa36765a6de5ca5fcf2987c
Kreoniz <kreoniz@mail.ru>
2023-01-16 18:13:45 +0600

    Add display for tracking operations

--- script.js
+++ script.js
@@ -34,7 +34,8 @@ let operation = "";
 
 const snarkyErrorMessage = "Weirdo...";
 
-const display = document.querySelector("#display");
+const displayNumber = document.querySelector("#displayNumber");
+const displayOperations = document.querySelector("#displayOperations");
 const digitButtons = document.querySelectorAll(".digit");
 const operatorButtons = document.querySelectorAll(".operator");
 const equalButton = document.querySelector(".equal");
@@ -43,7 +44,7 @@ const clearButton = document.querySelector(".clear");
 digitButtons.forEach(digit => {
     digit.addEventListener("click", e => {
         displayValue = +(displayValue + e.currentTarget.textContent);
-        display.textContent = displayValue;
+        displayNumber.textContent = displayValue;
     });
 });
 
@@ -55,13 +56,14 @@ operatorButtons.forEach(operator => {
             value = operate(operation, +value, +displayValue);
 
             value = Number.isFinite(value) ? Math.round(value * 100000) / 100000 : value;
-            display.textContent = value;
+            displayNumber.textContent = value;
             displayValue = "";
         } else if (Number.isFinite(displayValue)) {
             value = +displayValue;
             displayValue = "";
         }
         operation = e.currentTarget.textContent;
+        displayOperations.textContent = `${value} ${operation}`;
     });
 });
 
@@ -69,10 +71,11 @@ equalButton.addEventListener("click", e => {
     if (value == snarkyErrorMessage) value = 0;
 
     if (operation && Number.isFinite(displayValue)) { 
+        displayOperations.textContent += ` ${displayValue} =`;
         value = operate(operation, +value, +displayValue);
 
         value = Number.isFinite(value) ? Math.round(value * 100000) / 100000 : value;
-        display.textContent = value;
+        displayNumber.textContent = value;
         displayValue = "";
     } else if (Number.isFinite(displayValue)) {
         value = +displayValue;
@@ -85,5 +88,6 @@ clearButton.addEventListener("click", e => {
     displayValue = "";
     value = 0;
     operation = "";
-    display.textContent = value;
+    displayNumber.textContent = value;
+    displayOperations.textContent = "";
 });
